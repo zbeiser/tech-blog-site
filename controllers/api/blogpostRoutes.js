fc10844ad9ad6dae9563c7d92f99dbcddc1d0d6b
Zach Beiser <zbeiser@gmail.com>
2023-02-02 11:58:27 -0800

    wrote post '/blogpost/' and delete /blogpost/:id' routes

---
+++ controllers/api/blogpostRoutes.js
@@ -0,0 +1,40 @@
+const router = require('express').Router();
+const { Blogpost } = require('../../models');
+const withAuth = require('../../utils/auth');
+
+// Create a new blogpost
+router.post('/', withAuth, async (req, res) => {
+  try {
+    const newBlogpost = await Blogpost.create({
+      ...req.body,
+      user_id: req.session.user_id,
+    });
+
+    res.status(200).json(newBlogpost);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+// Delete a blogpost
+router.delete('/:id', withAuth, async (req, res) => {
+  try {
+    const blogpostData = await Blogpost.destroy({
+      where: {
+        id: req.params.id,
+        user_id: req.session.user_id,
+      },
+    });
+
+    if (!blogpostData) {
+      res.status(404).json({ message: 'No blogpost found with this id!' });
+      return;
+    }
+
+    res.status(200).json(blogpostData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
