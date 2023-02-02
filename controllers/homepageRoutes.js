67f6ec7fc40fee7e70246548b7d183fafe111296
Zach Beiser <zbeiser@gmail.com>
2023-02-01 19:53:39 -0800

    wrote get '/blogpost/:id' route

--- controllers/homepageRoutes.js
+++ controllers/homepageRoutes.js
@@ -25,4 +25,30 @@ router.get('/', async (req, res) => {
   }
 });
 
+// View one blogpost
+router.get('/blogpost/:id', async (req, res) => {
+  try {
+    const blogpostData = await Blogpost.findByPk(req.params.id, {
+      include: [
+        {
+          model: User,
+          attributes: ['username'],
+        },
+        {
+          model: Comment,
+        }
+      ],
+    });
+
+    const blogpost = blogpostData.get({ plain: true });
+
+    res.render('viewpost', {
+      ...blogpost,
+      logged_in: req.session.logged_in
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
 module.exports = router;
