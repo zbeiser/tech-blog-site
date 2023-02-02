77361c890a900fdc95465d07f30fbb6b08438f92
Zach Beiser <zbeiser@gmail.com>
2023-02-01 19:40:49 -0800

    wrote get 'dashboard/' route

--- controllers/dashboardRoutes.js
+++ controllers/dashboardRoutes.js
@@ -3,4 +3,30 @@ const router = express.Router();
 const { User, Blogpost, Comment } = require("../models");
 const withAuth = require("../utils/auth");
 
+// Get all of user's blog posts to dashboard
+router.get('/', withAuth, async (req, res) => {
+  try {
+    const blogpostData = await Blogpost.findAll({
+      where: {
+        user_id: req.session.user_id
+      },
+      include: [
+        {
+          model: User,
+          attributes: ['username', 'id']
+        },
+      ]
+    });
+
+    const blogposts = blogpostData.map((blogpost) => blogpost.get({ plain: true }));
+
+    res.render('dashboard', {
+      blogposts,
+      logged_in: true
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
 module.exports = router;
