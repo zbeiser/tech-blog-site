8fcb24618c1b69511f73471ec8f3bb1fb8b1fa80
Zach Beiser <zbeiser@gmail.com>
2023-02-09 13:19:28 -0800

    finally fixed the signup button. was throwing error since loginForm wasn't on the signup page. separated login and signup into 2 files

--- public/js/login.js
+++ public/js/login.js
@@ -23,27 +23,3 @@ document
   .querySelector('#loginForm')
   .addEventListener('submit', loginFormHandler);
 
-const signupFormHandler = async (event) => {
-  event.preventDefault();
-
-  const username = document.querySelector('#usernameSignup').value.trim();
-  const password = document.querySelector('#passwordSignup').value.trim();
-
-  if (username && password) {
-    const response = await fetch('/api/users/', {
-      method: 'POST',
-      body: JSON.stringify({ username, password }),
-      headers: { 'Content-Type': 'application/json' },
-    });
-
-    if (response.ok) {
-      document.location.replace('/dashboard');
-    } else {
-      alert(response.statusText);
-    }
-  }
-};
-
-document
-  .querySelector('#signupForm')
-  .addEventListener('submit', signupFormHandler);
